4bd7b15df8bcc07d1f74614cafc289cd28f50361
Yash Sangwan <yashsangwan00@gmail.com>
2024-07-16 11:52:36 +0530

    day 2

---
+++ Day-2/Day2.js
@@ -0,0 +1,78 @@
+// Arithmetic Operations
+
+// Task 1
+
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+// Task 2
+
+console.log(a - b);
+
+// Task 3
+
+console.log(a * b);
+
+// Task 4
+
+console.log(a / b);
+
+// Task 5
+
+console.log(a % b);
+
+//Assignment Operators
+
+// Task 6
+
+a += 2;
+
+// Task 7
+
+a -= 2;
+
+// Comparison Operators
+
+// Task 8
+
+console.log(a > b);
+console.log(a < b);
+
+// Task 9
+
+console.log(a >= b);
+console.log(a <= b);
+
+// Task 9
+
+console.log(a == b);
+console.log(a === b);
+
+// Logical Operators
+
+let c = 0;
+let d = 1;
+
+// Task 11
+
+console.log(c && d);
+
+// Task 12
+
+console.log(c || d);
+
+// Task 13
+
+console.log(!c);
+
+// Ternary Operator
+
+// Task 14
+
+let num = 10;
+
+let result = num > 0 ? "positive" : "negative";
+
+console.log(result);
+
---
+++ Day-2/Day2.md
@@ -0,0 +1,26 @@
+# Operators
+
+## Arithmetic Operations
+
+![alt text](/images/d2/d2a1.png)
+
+## Assignment Operators
+
+![alt text](/images/d2/d2a2.png)
+
+## Comparison Operations
+
+![alt text](/images/d2/d2a3.png)
+
+## Logical Operators
+
+![alt text](/images/d2/d2a4.png)
+
+## Ternary Operator
+
+![alt text](/images/d2/d2a5.png)
+
+## Feature Request
+
+![alt text](/images/d2/d2fr.png)
+
